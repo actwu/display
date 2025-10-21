eaf479f622347e02bdb08854de6553b18b88a95d
actwu <110190482+actwu@users.noreply.github.com>
2025-10-21 13:47:48 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='disp-v1'
-const FILES=['./','./index.html','./manifest.json']
+const CACHE='disp-v1.1'
+const FILES=['./','./404.html','./manifest.json']
 
 self.addEventListener('install',e=>{
 e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))
